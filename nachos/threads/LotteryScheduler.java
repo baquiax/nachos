f1d6b30ae735d83a9c7d2f669331c41cfcce82bd
Alexander Baquiax <josue.baquiax@fox.com>
2016-06-01 15:30:15 -0600

    Lottery advances

--- nachos/threads/LotteryScheduler.java
+++ nachos/threads/LotteryScheduler.java
@@ -27,22 +27,70 @@ import java.util.Iterator;
  * the maximum).
  */
 public class LotteryScheduler extends PriorityScheduler {
-    /**
-     * Allocate a new lottery scheduler.
-     */
-    public LotteryScheduler() {
-    }
+	/**
+	 * Allocate a new lottery scheduler.
+	 */
+	public LotteryScheduler() {
+	}
+	
+	/**
+	 * Allocate a new lottery thread queue.
+	 *
+	 * @param	transferPriority	<tt>true</tt> if this queue should
+	 *					transfer tickets from waiting threads
+	 *					to the owning thread.
+	 * @return	a new lottery thread queue.
+	 */
+	public ThreadQueue newThreadQueue(boolean transferPriority) {
+		// implement me		
+		return new LotteryQueue(transferPriority);
+	}
+	
+	protected ThreadState getThreadState(KThread thread) {
+		if (thread.schedulingState == null)
+			thread.schedulingState = new ThreadState(thread);
+
+		return (ThreadState) thread.schedulingState;
+	}
+	
+	protected class LotteryQueue extends ThreadQueue {
+		public booelan transferPriority;
+		LinkedList<KThread> waitQueue = new LinkedList<KThread>();
+		ThreadState lockHolder = null;
+		
+		public LotteryQueue(booelan transferPriority) {
+			this.transferPriority = transferPriority;
+		}
+		public void waitForAccess(KThread thread) {
+			Lib.assertTrue(Machine.interrupt().disabled());
+			ThreadState ts = getThreadState(thread).waitForAccess(this);
+			if (lockHolder != null) {
+				lockHolder.effectivePriority += ts.effectivePriority; 
+			}
+			this.waitQueue.add(ts);						
+		}
+		
+		public KThread nextThread() {
+			if (this.waitQueue.size() > 0) {
+				//Aplicar la loteria y retornar el elegido!
+				
+			}
+			return null;
+		}
     
-    /**
-     * Allocate a new lottery thread queue.
-     *
-     * @param	transferPriority	<tt>true</tt> if this queue should
-     *					transfer tickets from waiting threads
-     *					to the owning thread.
-     * @return	a new lottery thread queue.
-     */
-    public ThreadQueue newThreadQueue(boolean transferPriority) {
-	// implement me
-	return null;
-    }
+    	public void acquire(KThread thread) {
+			this.lockHolder = thread;			
+			if (this.transferPriority) {
+				int transferedPriority = 0;
+				for (int i = 0; i < waitQueue.size(); i++) {
+					transferedPriority = waitQueue.get(i).priority;					
+				}
+				this.lockHolder.effectivePriority = transferedPriority;
+			}
+		}
+
+    	public void print() {
+			
+		}
+	}			
 }
